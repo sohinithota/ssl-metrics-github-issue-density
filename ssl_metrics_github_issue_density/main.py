74d04b0d6661db3135fb5d6832ac488527915c40
Sohini Thota <sthota@luc.edu>
2021-10-07 18:49:57 -0500

    json files

--- ssl_metrics_github_issue_density/main.py
+++ ssl_metrics_github_issue_density/main.py
@@ -4,10 +4,8 @@ def getArgs() -> Namespace:
     parser: ArgumentParser = ArgumentParser(
         prog = "SSL Metrics Density Issues", usage = "Generate metrics based on Density issues"
     )
-    parser.add_argument(
-        "-j",
-        "--json",
-        required = True,
-        type = str,
-        help = "json file created through LOC"
+    parser.add_argument("-j", "--json", required = True, type = open, help = "json file created through LOC"
     )
+
+    args = parser.parse_args()
+    return args
