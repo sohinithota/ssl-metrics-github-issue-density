1bb11a36e58126ce127c2df2bd5e4efca7d074c8
Sohini Thota <sthota@luc.edu>
2021-10-10 12:00:19 -0500

    added importing data method for json file 'commits', will do next and add argparse for file 'issues'

--- ssl_metrics_github_issue_density/main.py
+++ ssl_metrics_github_issue_density/main.py
@@ -1,32 +1,33 @@
 import argparse
 import json
+import ast
 
 def getArgs():
     argparse1 = argparse.ArgumentParser(
         prog = "SSL Metrics Density Issues", usage = "Generate metrics based on Density issues"
     )
-    argparse1.add_argument("-j", "--json", required = True, type = open, help = "json file created through LOC"
+    argparse1.add_argument("-j", "--jsonfile1", required = True, type = open, help = "json file created through LOC"
     )
     args = argparse1.parse_args()
     return args
 
-def import_data():
-    f = open('issues.json',)
-    data = json.load(f)
-    for i in data[0]:
-        print(i)
-    f.close()
+#used this method in previous productivity project, it takes in the JSON as a list to be manipulated later. I store data for both files in this project.
+def import_data_commits(filename: str = "commits.json") -> list:
+    with open(file = filename, mode= "r") as fp:
+        return json.load(fp)
+
 
 
 def main():
     args = getArgs()
-    import_data()
+    jsonfile1 = args.jsonfile1.name
+    data = import_data_commits(jsonfile1)
 
 if __name__ == "__main__":
     main()
 
-'''
-## get values from both files:
-issues.json: created_at/updated_at/closed_at (for timeline), user (attached to each)
-commits.json: LOC_sum (module size),  day (for timeline)
-## create timeline of commits, in between put issues
+
+#get values from both files:
+#issues.json: created_at/updated_at/closed_at (for timeline), user (attached to each)
+#commits.json: LOC_sum (module size),  day (for timeline)
+# create timeline of commits, in between put issues
