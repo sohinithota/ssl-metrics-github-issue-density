9adc242bb63c5cba431c0d7616bcfe387d451bab
Sohini Thota <sthota@luc.edu>
2021-10-10 12:19:30 -0500

    added second methods for jsons

--- ssl_metrics_github_issue_density/main.py
+++ ssl_metrics_github_issue_density/main.py
@@ -2,26 +2,43 @@ import argparse
 import json
 import ast
 
-def getArgs():
+def getArgs1():
     argparse1 = argparse.ArgumentParser(
         prog = "SSL Metrics Density Issues", usage = "Generate metrics based on Density issues"
     )
-    argparse1.add_argument("-j", "--jsonfile1", required = True, type = open, help = "json file created through LOC"
+    argparse1.add_argument("-j", "--jsonfile1", required = True, type = open, help = "first json for this project- commits one.")
+   # argparse1.add_argument("-j", "--jsonfile2", required = True, type = open, help = "second json for this project- issues one.")
+    
+    args1 = argparse1.parse_args()
+    return args1
+    
+def getArgs2():
+    argparse1 = argparse.ArgumentParser(
+        prog = "SSL Metrics Density Issues", usage = "Generate metrics based on Density issues"
     )
-    args = argparse1.parse_args()
-    return args
+   # argparse1.add_argument("-j", "--jsonfile1", required = True, type = open, help = "first json for this project- commits one.")
+    argparse1.add_argument("-j", "--jsonfile2", required = True, type = open, help = "second json for this project- issues one.")
+    
+    args1 = argparse1.parse_args()
+    return args1
 
 #used this method in previous productivity project, it takes in the JSON as a list to be manipulated later. I store data for both files in this project.
 def import_data_commits(filename: str = "commits.json") -> list:
     with open(file = filename, mode= "r") as fp:
-        return json.load(fp)
+        return (json.load(fp))
 
+def import_data_issues(filename: str = "issues.json") -> list:
+    with open(file = filename, mode= "r") as fp:
+        return (json.load(fp))
 
 
 def main():
-    args = getArgs()
+    args = getArgs1()
     jsonfile1 = args.jsonfile1.name
     data = import_data_commits(jsonfile1)
+    args = getArgs2()
+    jsonfile2 = args.jsonfile2.name
+    data = import_data_issues(jsonfile2)
 
 if __name__ == "__main__":
     main()
