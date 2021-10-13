96dca47fa9b850225ce562c12643236b2e11f335
Sohini Thota <sthota@luc.edu>
2021-10-13 09:07:06 -0500

    got all of the values needed from both files

--- ssl_metrics_github_issue_density/main.py
+++ ssl_metrics_github_issue_density/main.py
@@ -7,12 +7,12 @@ def getArgs1():
         prog = "SSL Metrics Density Issues", usage = "Generate metrics based on Density issues"
     )
     argparse1.add_argument("-j", "--jsonfile1", required = True, type = open, help = "first json for this project- commits one.")
-   # argparse1.add_argument("-j", "--jsonfile2", required = True, type = open, help = "second json for this project- issues one.")
+    argparse1.add_argument("-j2", "--jsonfile2", required = True, type = open, help = "second json for this project- issues one.")
     
     args1 = argparse1.parse_args()
     return args1
     
-def getArgs2():
+# def getArgs2():
     argparse1 = argparse.ArgumentParser(
         prog = "SSL Metrics Density Issues", usage = "Generate metrics based on Density issues"
     )
@@ -36,11 +36,13 @@ def get_loc_sum_from_issues(file)-> list:
     return ([issues["loc_sum"] for issues in file])
 
 #get the day from issues file
-def get_day_from_issues(file)-> list:
+def get_day_from_commits(file)-> list:
     return ([issues["day"] for issues in file])
 
-def get_created_at_from_commits(file)-> list:
-    print ([commits["created_at"] for commits in file])
+def get_created_at_from_issues(file)-> list:
+    return ([commits["created_at"] for commits in file])
+    return ([commits["closed_at"] for commits in file])
+    return ([commits["updated_at"] for commits in file])
 
 # def get_created_at_from_commits(file)-> list:
     print (file[len(file)-1]["created_at"])
@@ -48,15 +50,16 @@ def get_created_at_from_commits(file)-> list:
 
 def main():
     args = getArgs1()
-    jsonfile1 = args.jsonfile1.name
+    jsonfile1 = args.jsonfile1.name #commits.json
     data1 = import_data_commits(jsonfile1)
-    get_created_at_from_commits(data1)
-    args = getArgs2()
-    jsonfile2 = args.jsonfile2.name
+    loc_sum = get_loc_sum_from_issues(data1)
+    get_day = get_day_from_commits(data1)
+   # args = getArgs2()
+    jsonfile2 = args.jsonfile2.name #issues.json
     data2 = import_data_issues(jsonfile2)
+    get_created_at_from_issues(data2)
     #get the value LOC_sum from commits.json
-    loc_sum = get_loc_sum_from_issues(data2)
-    get_day = get_day_from_issues(data2)
+    
 if __name__ == "__main__":
     main()
 
